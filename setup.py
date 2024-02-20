28e7dcd99aa14134a9e2c7c6981f7576caabd681
danoctua <danik4tl@gmail.com>
2024-02-20 12:10:20 +0100

    chore: Bump package version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Markdown',
-    version='0.1.0',
+    version='0.4',
     url='http://github.com/dcolish/flask-markdown',
     license='BSD',
     author='Dan Colish',
